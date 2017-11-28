d992053b404288b08185da22d16dd3911ed8501d
Gunter Grodotzki <gunter@grodotzki.co.za>
2017-11-28 09:36:53 +0200

    add maven support
    --

--- test.sh
+++ test.sh
@@ -67,7 +67,7 @@ consolelog "running role as playbook #1"
 ansible-playbook \
   --inventory="${TARGET_HOST}," \
   --user="${CONNECT_USER}" \
-  --extra-vars="role_root=${role_root} oracle_java_version=${ORACLE_JAVA_VERSION}" \
+  --extra-vars="role_root=${role_root} oracle_java_version=${ORACLE_JAVA_VERSION} maven_version=${MAVEN_VERSION}" \
   --connection="${CONNECTION}" \
   tests/test.yml
 
@@ -75,9 +75,11 @@ consolelog "running role as playbook #2"
 ansible-playbook \
   --inventory="${TARGET_HOST}," \
   --user="${CONNECT_USER}" \
-  --extra-vars="role_root=${role_root} oracle_java_version=${ORACLE_JAVA_VERSION}" \
+  --extra-vars="role_root=${role_root} oracle_java_version=${ORACLE_JAVA_VERSION} maven_version=${MAVEN_VERSION}" \
   --connection="${CONNECTION}" \
   tests/test.yml
 
 # "test"
 "$(dpkg -L oracle-java${ORACLE_JAVA_VERSION:0:1}-jdk | grep -e 'bin/java$' | tail -n1)" -version
+java -version
+mvn --version
